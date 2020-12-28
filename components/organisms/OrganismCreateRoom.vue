5c9fe1fd9bf98fdf342d4c6237a333096f65ccae
kght6123 <i.feeling.song@gmail.com>
2020-12-29 00:39:19 +0900

    ちょっと修正

--- components/organisms/OrganismCreateRoom.vue
+++ components/organisms/OrganismCreateRoom.vue
@@ -14,7 +14,7 @@
       class="flex mx-auto text-white bg-indigo-500 border-0 py-2 px-8 focus:outline-none hover:bg-indigo-600 rounded text-lg"
       >部屋に移動する</atom-button
     >
-    <atom-textbox-2 v-model="roomId" class="hidden" />
+    <atom-textbox v-model="roomId" class="hidden" />
   </div>
 </template>
 
